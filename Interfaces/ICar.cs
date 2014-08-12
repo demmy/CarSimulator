5cac5988225c6a41858ce21be48096c4af8e0286
demmy <demmy.uber@gmail.com>
2014-08-12 10:30:55 +0300

    add interfaces

---
+++ Interfaces/ICar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces.Parts;
+
+namespace Interfaces
+{
+    interface ICar
+    {
+        IEngine Engine { get; }
+        IPanel Panel { get; }
+        IPedal Pedal { get; }
+        IRudder Rudder { get; }
+        ITank Tank { get; }
+        ITransmission Transmission { get; }
+
+        int CurrentGear { get; }
+        double CurrentSpeed { get; }
+        double CurrentRudderDegree { get; }
+        double Fuel { get; }
+        bool HeadLight { get; }
+
+        void Accelerate(int pedalPressPower);
+        void Break(int pedalPressPower);
+
+        void TurnLeft(int degree);
+        void TurnRight(int degree);
+
+        void LightSwitch();
+    }
+}
---
+++ Interfaces/Parts/IPanel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Parts
+{
+    interface IPanel
+    {
+        string Type { get; }
+        string Speed { get; }
+        string Fuel { get; }
+        string Degree { get; }
+        string Gear { get; }
+        string Light { get; }
+        string MaxSpeed { get; }
+        string MaxGear { get; }
+        string PedalLuft { get; }
+        string RudderLuft { get; }
+    }
+}
